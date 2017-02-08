6002af1d3d7dade3f479d57b3bc8ce75171675c6
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2017-02-07 18:29:26 -0800

    add session helpers

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
 helpers do 
+
+	def current_user
+		return nil unless session[:user_id]
+		@current_user ||= User.find(session[:user_id])
+	end 
+
+	def login
+		session[:user_id] = @user.id
+	end 
+
+	def logout
+		session.destroy
+	end 
+
 end 
